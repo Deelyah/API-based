aceb6acaaa58ccd495e28aa5a31f7246aaef11e7
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2022-02-19 09:40:47 +0100

    initial coommit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import TodoList from '@/components/TodoList.vue'
-import CreateTodoList from '@/components/CreateTodoList.vue'
-import Todos from '@/components/Todos.vue'
+import TodoList from '../components/TodoList.vue'
+import CreateTodoList from '../components/CreateTodoList.vue'
+import Todos from '../components/Todos.vue'
 
 Vue.use(Router)
 
